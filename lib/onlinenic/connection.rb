7000b3468373a37440298836cac6a2b4691953d9
İzzet Emre Kutlu <emrekutlu@gmail.com>
2009-12-09 11:42:51 +0200

    :auto_close option is added to Connection

--- lib/onlinenic/connection.rb
+++ lib/onlinenic/connection.rb
@@ -3,15 +3,20 @@ module Onlinenic
 
     attr_reader :response
 
-    def initialize
+    def initialize(opts={})
+      @opts = { :auto_logout => true }.merge(opts)
       @wrapper = Onlinenic::Wrapper.new
     end
 
     def check_domain(domain)
       domain = Onlinenic::Domain.new(domain)
       @response = @wrapper.check_domain({ :domain => domain.full_name, :domaintype => domain.type })
-      @wrapper.logout
+      logout if @opts[:auto_close]
       @response.get_data("avail").eql?("1") ? true : false
+    end
+
+    def logout
+      @wrapper.logout
     end
 
   end
